85d1c6e591b187757937bf821b2ef8e784cac44c
Sasha <dzeromuolter@gmail.com>
2024-03-04 10:40:06 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,6 @@
+players = ['Ashleigh Barty', 'Simona Halep', 'Naomi Osaka', 'Karolina Pliskova', 'Elina Svitolina']
+# Міняємо місцями першого гравця з останнім.
+players[0], players[-1] = players[-1], players[0]
+
+print(players)
+
